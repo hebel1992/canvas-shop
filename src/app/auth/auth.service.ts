20fc16069335f722ea2ff42ed537c799f073618c
hebel1992 <bartosz.hebrycz@gmail.com>
2020-06-18 20:08:33 +0200

    register and create user switched to async await

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,9 +4,9 @@ import {Router} from '@angular/router';
 import {UserService} from '../user/user-service';
 import {BasketService} from '../basket/basket-service';
 import * as firebase from 'firebase';
-import {BasketItemModel} from '../basket/basket-item.model';
 import {AngularFirestore} from '@angular/fire/firestore';
 import {UserDbService} from '../user/user-db-service';
+import {BasketItemModel} from '../basket/basket-item.model';
 
 @Injectable({
   providedIn: 'root'
@@ -29,13 +29,16 @@ export class AuthService {
     });
   }
 
-  register(email: string, password: string) {
+  async register(email: string, password: string) {
     const session = firebase.auth.Auth.Persistence.SESSION;
-    return this.auth.setPersistence(session).then(() => {
-      return this.auth.createUserWithEmailAndPassword(email, password).then(cred => {
-        return this.createUserProfile(cred);
-      });
-    });
+
+    try {
+      await this.auth.setPersistence(session);
+      const userCredential = await this.auth.createUserWithEmailAndPassword(email, password);
+      await this.createUserProfile(userCredential);
+    } catch (err) {
+      throw new Error(err);
+    }
   }
 
   logout() {
@@ -49,16 +52,18 @@ export class AuthService {
     return this.auth.sendPasswordResetEmail(email);
   }
 
-   private  createUserProfile(cred: firebase.auth.UserCredential) {
-    const userRef = this.db.collection('users').doc(cred.user.uid).ref;
-    return userRef.get().then(docSnapshot => {
-      if (!docSnapshot.exists) {
+  private async createUserProfile(cred: firebase.auth.UserCredential) {
+    try {
+      const userRef = await this.db.collection('users').doc(cred.user.uid).ref.get();
+
+      if (!userRef.exists) {
         let localBasket: BasketItemModel[] = [];
         if (localStorage.getItem('basket')) {
           localBasket = JSON.parse(localStorage.getItem('basket'));
           localStorage.removeItem('basket');
         }
-        this.db.collection('users').doc(cred.user.uid).set({
+        await this.db.collection('users').doc(cred.user.uid).set({
+          id: cred.user.uid,
           firstName: '',
           lastName: '',
           email: cred.user.email,
@@ -74,6 +79,9 @@ export class AuthService {
       } else {
         localStorage.removeItem('basket');
       }
-    }).then(() => this.userDbService.fetchUserData(cred.user.uid));
+      await this.userDbService.fetchUserData(cred.user.uid);
+    } catch (err) {
+      throw new Error(err);
+    }
   }
 }
