4e78c7c80ed3d84fb1f5609a201b15829c923ee2
hebel1992 <bartosz.hebrycz@gmail.com>
2020-06-18 16:17:40 +0200

    reset password logic finished

--- src/app/auth/reset-password/reset-password.component.ts
+++ src/app/auth/reset-password/reset-password.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {faLock} from '@fortawesome/free-solid-svg-icons';
+import {NgModel} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {AngularFireAuth} from '@angular/fire/auth';
 
 @Component({
   selector: 'app-reset-password',
@@ -6,10 +10,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reset-password.component.css']
 })
 export class ResetPasswordComponent implements OnInit {
+  faPassword = faLock;
 
-  constructor() { }
+  mode: string;
+  actionCode: string;
+  actionCodeChecked = false;
+  error: string;
+  success: string;
+  isLoading = false;
 
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private auth: AngularFireAuth) {
   }
 
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      if (!params) {
+        this.router.navigate(['gallery']);
+      }
+
+      this.mode = params['mode'];
+      this.actionCode = params['oobCode'];
+
+      if (this.mode === 'resetPassword') {
+        this.auth.verifyPasswordResetCode(this.actionCode).then(email => {
+          this.actionCodeChecked = true;
+        }).catch(err => {
+          this.error = err.message;
+        });
+      } else {
+        this.error = 'An error occurred. Please try again';
+      }
+    });
+  }
+
+  onResetPasswordSubmit(password: NgModel, password2: NgModel) {
+    this.isLoading = true;
+    if (password.value !== password2.value) {
+      this.error = 'Passwords do not match';
+      this.isLoading = false;
+      return;
+    }
+
+    this.auth.confirmPasswordReset(this.actionCode, password.value).then(resp => {
+      this.error = null;
+      this.success = 'Password has been changed. You can now log in.';
+      this.isLoading = false;
+      setTimeout(() => {
+        this.router.navigate(['/gallery']);
+      }, 2500);
+    }).catch(err => {
+      this.error = err.message;
+      this.isLoading = false;
+    });
+  }
 }
